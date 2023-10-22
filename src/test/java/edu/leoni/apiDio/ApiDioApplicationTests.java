d671049abe396f6a3b218923b7f1e70959701ac8
Leoni Kaian <leonikfcdegois@gmail.com>
2023-10-22 16:48:59 -0300

    Spring Initializr

---
+++ src/test/java/edu/leoni/apiDio/ApiDioApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.leoni.apiDio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
